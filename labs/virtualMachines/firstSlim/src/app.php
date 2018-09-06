48d8201b91711d222058f3364cf5daa09165e1e3
Jim Feather <jim@indrez.com>
2018-09-05 23:31:42 -0400

    Creating slides and activities for Thursday’s class

--- labs/virtualMachines/firstSlim/src/app.php
+++ labs/virtualMachines/firstSlim/src/app.php
@@ -54,6 +54,50 @@ class App
          return $response;
 
      });
+     $app->post('/people', function (Request $request, Response $response) {
+         $this->logger->addInfo("POST /people/");
+
+         // check that peron exists
+         // $person = $this->db->query('SELECT * from people where id='.$id)->fetch();
+         // if(!$person){
+         //   $errorData = array('status' => 404, 'message' => 'not found');
+         //   $response = $response->withJson($errorData, 404);
+         //   return $response;
+         // }
+
+         // build query string
+         $createString = "INSERT INTO people ";
+         $fields = $request->getParsedBody();
+         $keysArray = array_keys($fields);
+         $last_key = end($keysArray);
+         $values = '(';
+         $fieldNames = '(';
+         foreach($fields as $field => $value) {
+           $values = $values . "'"."$value"."'";
+           $fieldNames = $fieldNames . "$field";
+           if ($field != $last_key) {
+             // conditionally add a comma to avoid sql syntax problems
+             $values = $values . ", ";
+             $fieldNames = $fieldNames . ", ";
+           }
+         }
+         $values = $values . ')';
+         $fieldNames = $fieldNames . ') VALUES ';
+         $createString = $createString . $fieldNames . $values . ";";
+         // execute query
+         try {
+           $this->db->exec($createString);
+         } catch (\PDOException $e) {
+           var_dump($e);
+           $errorData = array('status' => 400, 'message' => 'Invalid data provided to create person');
+           return $response->withJson($errorData, 400);
+         }
+         // return updated record
+         $person = $this->db->query('SELECT * from people ORDER BY id desc LIMIT 1')->fetch();
+         $jsonResponse = $response->withJson($person);
+
+         return $jsonResponse;
+     });
      $app->put('/people/{id}', function (Request $request, Response $response, array $args) {
          $id = $args['id'];
          $this->logger->addInfo("PUT /people/".$id);
--- labs/virtualMachines/slimClient/src/app.php
+++ labs/virtualMachines/slimClient/src/app.php
@@ -2,12 +2,22 @@
 namespace feather\firstSlimClient;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\PhpRenderer;
+
 require './vendor/autoload.php';
 
 class App
 {
-
    private $app;
+   private const SCRIPT_INCLUDE = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <script
+     src="https://code.jquery.com/jquery-3.3.1.min.js"
+     integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+     crossorigin="anonymous"></script>
+   </head>
+   <script src=".public/script.js"></script>';
+
+
    public function __construct() {
 
      $app = new \Slim\App(['settings' => $config]);
@@ -20,6 +30,8 @@ class App
          $logger->pushHandler($file_handler);
          return $logger;
      };
+     $container['renderer'] = new PhpRenderer("./templates");
+
      function makeApiRequest($path){
        $ch = curl_init();
 
@@ -31,22 +43,55 @@ class App
        $response = curl_exec($ch);
        return json_decode($response, true);
      }
-     $app->get('/', function (Request $request, Response $response) {
+     $app->get('/', function (Request $request, Response $response, array $args) {
        $responseRecords = makeApiRequest('people');
-       $tableRows = "<tr><th>Name</th><th>Age</th><th>Occupation<th>Actions</th></th>";
+       $tableRows = "";
        foreach($responseRecords as $person) {
          $tableRows = $tableRows . "<tr>";
          $tableRows = $tableRows . "<td>".$person["name"]."</td><td>".$person["age"]."</td><td>".$person["occupation"]."</td>";
+         $tableRows = $tableRows . "<td>
+         <a href='http://localhost:8080/slimClient/people/".$person["id"]."' class='btn btn-primary'>View Details</a>
+         <a href='http://localhost:8080/slimClient/people/".$person["id"]."/edit' class='btn btn-secondary'>Edit</a>
+         <a data-id='".$person["id"]."' class='btn btn-danger deletebtn'>Delete</a>
+
+         </td>";
          $tableRows = $tableRows . "</tr>";
        }
-       $body = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">';
 
-       $body = $body . "<h1>People</h1>
-       <table class='table'>
-       $tableRows
-       </table>";
-       $response->write($body);
+       $templateVariables = [
+           "title" => "People",
+           "tableRows" => $tableRows
+       ];
+       return $this->renderer->render($response, "/people.html", $templateVariables);
+     });
+     $app->get('/people/add', function(Request $request, Response $response) {
+       $templateVariables = [
+         "type" => "new",
+         "title" => "Create Person"
+       ];
+       return $this->renderer->render($response, "/peopleForm.html", $templateVariables);
+
+     });
+
+     $app->get('/people/{id}', function (Request $request, Response $response, array $args) {
+         $id = $args['id'];
+         $responseRecords = makeApiRequest('people/'.$id);
+         $body = "<h1>Name: ".$responseRecords['name']."</h1>";
+         $body = $body . "<h2>Occupation: ".$responseRecords['occuptation']."</h2>";
+         $body = $body . "<h3>Age: ".$responseRecords['age']."</h3>";
+         $response->getBody()->write($body);
          return $response;
+     });
+     $app->get('/people/{id}/edit', function (Request $request, Response $response, array $args) {
+         $id = $args['id'];
+         $responseRecord = makeApiRequest('people/'.$id);
+         $templateVariables = [
+           "type" => "edit",
+           "title" => "Edit User",
+           "person" => $responseRecord
+         ];
+         return $this->renderer->render($response, "/peopleEditForm.html", $templateVariables);
+
      });
      $this->app = $app;
    }
